59177390d23be4226135d86b86f71a66e1994b0b
Christian <christian.stippel@gmail.com>
2022-05-15 19:37:16 +0200

    refactor PoseEstimator

--- conv_recognition/head_pose.py
+++ conv_recognition/head_pose.py
@@ -5,19 +5,19 @@ import torch
 from PIL import Image
 import conv_recognition.utils as utils
 import numpy as np
-
+import copy
 class PoseEstimator:
 
-    def __init__(self, gpu=0, snapshot_path = 'data/models/6DRepNet_300W_LP_AFLW2000.pth'):
+    def __init__(self, gpu=0, snapshot_path='data/models/6DRepNet_300W_LP_AFLW2000.pth'):
         self.transformations = transforms.Compose([transforms.Resize(224),
                                                    transforms.CenterCrop(224),
                                                    transforms.ToTensor(),
                                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         self.detector = RetinaFace(gpu_id=gpu)
         self.model = SixDRepNet(backbone_name='RepVGG-B1g2',
-                           backbone_file='',
-                           deploy=True,
-                           pretrained=False)
+                                backbone_file='',
+                                deploy=True,
+                                pretrained=False)
         saved_state_dict = torch.load(snapshot_path, map_location='cpu')
         if 'model_state_dict' in saved_state_dict:
             self.model.load_state_dict(saved_state_dict['model_state_dict'])
@@ -29,36 +29,53 @@ class PoseEstimator:
 
     def get_poses(self, frame):
         faces = self.detector(frame)
-        for box, landmarks, score in faces:
-            if score < 0.95:
-                continue
-            x_min = int(box[0])
-            y_min = int(box[1])
-            x_max = int(box[2])
-            y_max = int(box[3])
-            bbox_width = abs(x_max - x_min)
-            bbox_height = abs(y_max - y_min)
+        results = []
+        with torch.no_grad():
+            for box, landmarks, score in faces:
+                if score < 0.95:
+                    continue
+                x_min = int(box[0])
+                y_min = int(box[1])
+                x_max = int(box[2])
+                y_max = int(box[3])
+                bbox_width = abs(x_max - x_min)
+                bbox_height = abs(y_max - y_min)
 
-            x_min = max(0, x_min-int(0.2*bbox_height))
-            y_min = max(0, y_min-int(0.2*bbox_width))
-            x_max = x_max+int(0.2*bbox_height)
-            y_max = y_max+int(0.2*bbox_width)
+                x_min = max(0, x_min-int(0.2*bbox_height))
+                y_min = max(0, y_min-int(0.2*bbox_width))
+                x_max = x_max+int(0.2*bbox_height)
+                y_max = y_max+int(0.2*bbox_width)
 
-            img = frame[y_min:y_max, x_min:x_max]
-            img = Image.fromarray(img)
-            img = img.convert('RGB')
-            img = self.transformations(img)
+                img = frame[y_min:y_max, x_min:x_max]
+                img = Image.fromarray(img)
+                img = img.convert('RGB')
+                img = self.transformations(img)
 
-            img = torch.Tensor(img[None, :]).cuda(self.gpu)
+                img = torch.Tensor(img[None, :]).cuda(self.gpu)
 
-            pred = self.model(img)
-            euler = utils.compute_euler_angles_from_rotation_matrices(pred)*180/np.pi
+                pred = self.model(img)
+                euler = utils.compute_euler_angles_from_rotation_matrices(
+                    pred)*180/np.pi
 
-            p_pred_deg = euler[:, 0].cpu()
-            y_pred_deg = euler[:, 1].cpu()
-            r_pred_deg = euler[:, 2].cpu()
+                pitch = euler[:, 0].cpu()
+                yaw = euler[:, 1].cpu()
+                roll = euler[:, 2].cpu()
 
-            utils.plot_pose_cube(frame,  y_pred_deg, p_pred_deg, r_pred_deg, x_min + int(.5*(
-                    x_max-x_min)), y_min + int(.5*(y_max-y_min)), size=bbox_width)
+                center_x = x_min + int(.5*(x_max-x_min))
+                center_y = y_min + int(.5*(y_max-y_min))
 
+                results.append((center_x, center_y, bbox_width, yaw, pitch, roll))
+        return results
+
+    def draw_results(self, frame, head_poses):
+        frame = copy(frame)
+        for center_x, center_y, size, yaw, pitch, roll in head_poses:
+            utils.plot_pose_cube(
+                    frame,  yaw, pitch, roll,
+                    center_x,
+                    center_y,
+                    size=size)
         return frame
+
+    def get_looking_direction(self, frame):
+        return
